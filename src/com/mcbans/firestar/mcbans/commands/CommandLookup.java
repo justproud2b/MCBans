20cd88b892a2c888e7a6eabbd4dc8400098f4560
Jon <jjulien@me.com>
2013-02-04 00:28:48 -0600

    Update src/com/mcbans/firestar/mcbans/commands/CommandLookup.java

--- src/com/mcbans/firestar/mcbans/commands/CommandLookup.java
+++ src/com/mcbans/firestar/mcbans/commands/CommandLookup.java
@@ -37,6 +37,6 @@ public class CommandLookup extends BaseCommand{
 
     @Override
     public boolean permission(CommandSender sender) {
-        return Perms.LOOKUP_PLAYER.has(sender);
+        sender.sendMessage Perms.LOOKUP_PLAYER.has(sender);
     }
 }
